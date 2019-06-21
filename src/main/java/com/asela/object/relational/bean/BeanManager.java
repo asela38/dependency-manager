e845997223c35dd07485f6aa17d5dae86060d189
asela <asela38@gmail.com>
2019-06-21 20:58:23 +0800

    fix errors

--- src/main/java/com/asela/object/relational/bean/BeanManager.java
+++ src/main/java/com/asela/object/relational/bean/BeanManager.java
@@ -5,6 +5,7 @@ import com.asela.object.relational.anno.Provides;
 import com.asela.object.relational.provider.H2ConnectionProvider;
 import com.sun.tools.javac.util.List;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -31,8 +32,9 @@ public class BeanManager {
                     Class<?> returnType = aMethod.getReturnType();
                     Supplier<?> supplier = () -> {
                         try {
-                            if (Modifier.isStatic(aMethod.getModifiers())) {
+                            if (!Modifier.isStatic(aMethod.getModifiers())) {
                                 Object object = aClass.getConstructor().newInstance();
+                                aMethod.setAccessible(true );
                                 return aMethod.invoke(object);
                             } else
                                 return aMethod.invoke(null);
@@ -42,7 +44,7 @@ public class BeanManager {
                         }
                     };
 
-                    registry.put(aClass, supplier);
+                    registry.put(returnType, supplier);
                 }
             }
         }
@@ -51,7 +53,10 @@ public class BeanManager {
     public <T, U> T getInstance(Class<T> aClass, Class<U> type) {
 
         try {
-            T t = aClass.getConstructor().newInstance(type);
+            Constructor<?>[] declaredConstructor = aClass.getDeclaredConstructors();
+            declaredConstructor[0].setAccessible(true);
+            T t = (T) declaredConstructor[0].newInstance(type);
+
             for (Field field : aClass.getDeclaredFields()) {
                 Inject inject = field.getAnnotation(Inject.class);
                 if (inject != null) {
